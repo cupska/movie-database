ab3f33d879a146cb2385fe783da06c0c23d78631
Muhammad Yusuf <my.kurn@gmail.com>
2023-02-27 22:39:46 +0700

    mantap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className=' bg-red-300 text-3xl text-green-500'>
-          Edit <code>src/App.js</code> and save to relaoad.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    );
 }
 
 export default App;
