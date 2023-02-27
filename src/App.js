c4557004fa737a1c5e6f4cae3ad26bad091da81e
Muhammad Yusuf <my.kurn@gmail.com>
2023-02-27 21:45:56 +0700

    mantap

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className=' bg-red-300 text-3xl'>
+          Edit <code>src/App.js</code> and save to relaoad.
         </p>
         <a
           className="App-link"
